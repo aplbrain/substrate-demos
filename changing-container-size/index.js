dbe94f25eda900e9b381e0313e2734d76983cdba
Jordan Matelsky <j6k4m8@gmail.com>
2017-09-07 13:37:03 -0400

    Update container size

--- changing-container-size/index.js
+++ changing-container-size/index.js
@@ -14,11 +14,20 @@ let V = new Visualizer({
         axis: new Axes(),
     },
     targetElement: "render-target",
+    // Start with width and height of 500px:
     width: 500,
     height: 500
 });
 
 V.triggerRender();
+
+window.addEventListener('keyup', ev => {
+    // Resize the visualizer to a random size:
+    V.resize(
+        Math.random() * window.innerWidth,
+        Math.random() * window.innerHeight
+    );
+});
 
 
 window.V = V;
